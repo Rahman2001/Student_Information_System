921e044abcdd0dc5079525290e7a48445712e7cf
Rahman <rahmanrejepov777@gmail.com>
2022-01-10 21:59:26 +0300

    Corrected an instance variable

--- src/gazi/university/Person_SubClasses/Employee_SubClasses/Academics_SubClasses/AssistingStaff.java
+++ src/gazi/university/Person_SubClasses/Employee_SubClasses/Academics_SubClasses/AssistingStaff.java
@@ -1,17 +1,18 @@
 package gazi.university.Person_SubClasses.Employee_SubClasses.Academics_SubClasses;
 
+import gazi.university.Location_SubClasses.Lab;
 import gazi.university.Person_SubClasses.Employee_SubClasses.Academics;
 
 public class AssistingStaff extends Academics {
-    private boolean hasLab = false;
+    private Lab lab ;
 
     public AssistingStaff(String identity_no, String name, String registry_number){
         super(identity_no, name, registry_number);
     }
-    public void addLab(){
-        this.hasLab = true;
+    public void addLab(Lab lab){
+        this.lab = lab;
     }
-    public boolean hasLab(){
-        return this.hasLab;
+    public Lab getLab(){
+        return this.lab;
     }
 }
