7e869e1baf73470f5656734ece066967eac663f1
Rahman <rahmanrejepov777@gmail.com>
2022-01-10 16:59:20 +0300

    98% is completed

--- src/gazi/university/Person_SubClasses/Employee_SubClasses/MaintanenceStaff_SubClasses/CareTaker.java
+++ src/gazi/university/Person_SubClasses/Employee_SubClasses/MaintanenceStaff_SubClasses/CareTaker.java
@@ -1,8 +1,35 @@
 package gazi.university.Person_SubClasses.Employee_SubClasses.MaintanenceStaff_SubClasses;
 
+import gazi.university.Location;
 import gazi.university.Person_SubClasses.Employee_SubClasses.MaintenanceStaff;
 
+import java.awt.*;
+import java.awt.geom.Area;
+import java.util.Set;
 
-public class CareTaker  {
+public class CareTaker extends MaintenanceStaff{
+    private static final int minAmountResponsibility = 3;
+    private int takenAmountResponsibility;
+    private int takenAmountSpace;
+    private static final int minIndoorSpace = 250; // m^2 unit
+
+    public CareTaker(String identity_no, String name, String registry_number){
+        super(identity_no, name, registry_number);
+    }
+
+    public boolean earnedHisSalary(){
+        return this.takenAmountSpace >= minIndoorSpace && this.takenAmountResponsibility >= minAmountResponsibility;
+    }
+
+    public void addResponsibility(MaintenanceStaff maintenanceStaff, Location location){
+        String maintenanceType = maintenanceStaff.getClass().getSimpleName();
+        if(maintenanceType.equalsIgnoreCase(this.getClass().getSimpleName())){
+            super.addResponsibility(maintenanceStaff, location);
+            this.takenAmountResponsibility++;
+            //this.takenAmountSpace += location.getSpace();
+        }else{
+            System.out.println("Incorrect type of maintenance! (should be : " + this.getClass().getSimpleName() + ")\n");
+        }
+    }
 
 }
--- src/gazi/university/Person_SubClasses/Employee_SubClasses/MaintanenceStaff_SubClasses/Gardener.java
+++ src/gazi/university/Person_SubClasses/Employee_SubClasses/MaintanenceStaff_SubClasses/Gardener.java
@@ -1,4 +1,30 @@
 package gazi.university.Person_SubClasses.Employee_SubClasses.MaintanenceStaff_SubClasses;
 
-public class Gardener {
+import gazi.university.Location;
+import gazi.university.Person_SubClasses.Employee_SubClasses.MaintenanceStaff;
+
+public class Gardener extends MaintenanceStaff {
+    private static final int minAmountResponsibility = 2;
+    private static final int minAmountSpace = 1000; //m^2 unit
+    private int takenAmountResponsibility;
+    private int takenAmountSpace;
+
+    public Gardener(String identity_no, String name, String registry_number){
+        super(identity_no, name, registry_number);
+    }
+
+    public boolean earnedHisSalary(){
+        return this.takenAmountSpace >= minAmountSpace && this.takenAmountResponsibility >= minAmountResponsibility;
+    }
+
+    public void addResponsibility(MaintenanceStaff maintenanceStaff, Location location){
+        String maintenanceType = maintenanceStaff.getClass().getSimpleName();
+        if(maintenanceType.equalsIgnoreCase(this.getClass().getSimpleName())){
+            super.addResponsibility(maintenanceStaff, location);
+            this.takenAmountResponsibility++;
+           // this.takenAmountSpace += location.getSpace();
+        }else{
+            System.out.println("Incorrect type of maintenance! (should be " + this.getClass().getSimpleName() + ")\n");
+        }
+    }
 }
